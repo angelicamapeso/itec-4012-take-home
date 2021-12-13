5a696883602bbbe53db57f1f74e9e815b324705f
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-12-12 22:00:26 -0500

    add router to App with pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./normalize.css";
+import './App.scss';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/me" element={<Profile />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
