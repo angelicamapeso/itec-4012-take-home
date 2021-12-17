78eef8f8870322bff99079725f5fe682364f5526
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-12-16 22:12:21 -0500

    remove dummy data from context

--- src/context/woopsContext.js
+++ src/context/woopsContext.js
@@ -6,11 +6,7 @@ const WoopsContext = React.createContext({
 });
 
 export const WoopsContextProvider = ({ children }) => {
-  const [woops, setWoops] = useState([
-    { email: 'johndoe@email.com', text: 'A sample mistake!' },
-    { email: 'jane@email.com', text: 'Another sample mistake!' },
-    { email: 'angie@email.com', text: 'Woah I did this!' },
-  ]);
+  const [woops, setWoops] = useState([]);
 
   const initializeWoops = (woopsFromApi) => {
     setWoops(woopsFromApi);
