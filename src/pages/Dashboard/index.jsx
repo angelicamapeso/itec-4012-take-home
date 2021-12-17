0189ac3d9fdaf4c67822a6f6072abc4dae079b7a
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-12-16 22:12:11 -0500

    load data from api to feed

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,6 @@
 import "./styles.scss";
 
-import { useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 
 import WoopsContext from "../../context/woopsContext";
 
@@ -8,12 +8,38 @@ import WoopsBuilder from "../../components/WoopsBuilder";
 import WoopsFeed from "../../components/WoopsFeed";
 
 export default function Dashboard() {
+  const [ feed, setFeed ] = useState([]);
   const woopsData = useContext(WoopsContext);
+
+  useEffect(() => {
+    if (woopsData.woops.length > 0) {
+      setFeed(woopsData.woops);
+    } else {
+      getWoopData();
+    }
+  }, []);
+
+  const getWoopData = async () => {
+    try {
+      const response = await fetch('https://firestore.googleapis.com/v1/projects/woops-store/databases/(default)/documents/woops/');
+      const data = await response.json();
+      const fData = data.documents.map(({
+        fields: {
+          text: { stringValue: textVal },
+          email: { stringValue: emailVal },
+        }
+      }) => ({ email: emailVal, text: textVal }));
+      setFeed(fData);
+      woopsData.initializeWoops(fData);
+    } catch(error) {
+      console.error(error);
+    }
+  }
 
   return (
     <div className="dashboard-wrapper">
       <WoopsBuilder />
-      <WoopsFeed woopsList={woopsData.woops} />
+      <WoopsFeed woopsList={feed} />
     </div>
   );
 }
