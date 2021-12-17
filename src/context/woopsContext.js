aa1045ebebd0a37127bc51676088156c6adc5d68
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-12-16 22:31:41 -0500

    add sorting function for api data

--- src/context/woopsContext.js
+++ src/context/woopsContext.js
@@ -2,21 +2,37 @@ import React, { useState } from "react";
 
 const WoopsContext = React.createContext({
   woops: [],
+  sortWoops: () => { },
   initializeWoops: () => { },
+  addWoop: () => { },
 });
 
 export const WoopsContextProvider = ({ children }) => {
   const [woops, setWoops] = useState([]);
 
+  const sortWoops = (woopsFromApi) => {
+    const data = [...woopsFromApi];
+    data.sort((a, b) => {
+      return new Date(b.updateTime).getTime() - new Date(a.updateTime).getTime()
+    });
+    return data;
+  }
+
   const initializeWoops = (woopsFromApi) => {
     setWoops(woopsFromApi);
+  }
+
+  const addWoop = (newWoop) => {
+    setWoops([newWoop, ...woops]);
   }
 
   return (
     <WoopsContext.Provider
       value={{
         woops,
+        sortWoops,
         initializeWoops,
+        addWoop,
       }}
     >
       {children}
